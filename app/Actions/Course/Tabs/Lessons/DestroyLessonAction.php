fc1d5f8a71c7f8b608de994e3ac1ac3f802ea991
Neo <neo@MacBook-Air-neo.local>
2024-12-13 11:10:40 +0500

    feat: lessons section has been added

--- app/Actions/Course/Tabs/Lessons/DestroyLessonAction.php
+++ app/Actions/Course/Tabs/Lessons/DestroyLessonAction.php
@@ -6,6 +6,11 @@ use App\Models\Lesson;
 
 class DestroyLessonAction
 {
+    /**
+     * Deletes a lesson.
+     *
+     * @param Lesson $lesson The lesson to be deleted.
+     */
     public function execute(Lesson $lesson): void
     {
         $lesson->delete();
--- app/Actions/Course/Tabs/Lessons/GetAllLessonsAction.php
+++ app/Actions/Course/Tabs/Lessons/GetAllLessonsAction.php
@@ -6,6 +6,12 @@ use App\Models\Lesson;
 
 class GetAllLessonsAction
 {
+/**
+ * Retrieves all lessons for the specified chapter.
+ *
+ * @param string $chapterId The ID of the chapter whose lessons are to be retrieved.
+ * @return array An array of lessons associated with the given chapter ID.
+ */
     public function execute(string $chapterId): array
     {
         $lessons = Lesson::where('course_chapter_id', $chapterId)->get();
